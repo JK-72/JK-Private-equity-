9896992da309929a24a8d174c00c9c0a4ddd9648
JK-72 <clientacquisitionhub1@gmail.com>
2026-02-27 15:57:18 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript for the 'Explore More' button hover effect
+document.querySelector('.explore-btn').addEventListener('click', () => {
+  alert("Thank you for exploring JK-72 with us!");
+});
